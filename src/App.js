1c4984ad9f06b419789b037fbf835aabf18af104
Wael Youssfi <youssfi.wael1997@gmail.com>
2021-09-29 21:39:58 +0100

    trying out the react hook form instead of formik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("firsname")} />
+            <input {...register("lastname")} />
+            <input type="submit"></input>
+        </form>
+    );
 }
-
 export default App;
